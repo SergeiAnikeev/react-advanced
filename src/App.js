5a1e01579c7f522a36bfdc963867e74aa618ef3c
SergeiAnikeev <anikeevsu@gmail.com>
2022-05-01 13:28:00 +1200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ErrorExample from './ErrorExample.js'
+
+function App() {
+  return (
+    <div className='container'>
+      <ErrorExample />
+    </div>
+  )
+}
+
+export default App
